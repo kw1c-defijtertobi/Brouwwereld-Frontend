11ef131e899db48dc5d4a7d7521f74f8b068019c
Tobi de Fijter <tobi.defijter@edu-kw1c.nl>
2022-03-16 11:25:36 +0100

    products

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;500;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;500;900&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 
 @import "./styles/variables.scss";
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,115 +1,191 @@
 <template>
-    <main>
-        <div class="filter">
-
-
-        </div>
+    <main class="productpage">
+        <aside>
+            <h2>Bier</h2>
+            <div class="brands">
+                <p>Merken</p>
+                <div class="filters">
+                    <input type="checkbox" id="filter1" name="filter1" value="Hertog jan">
+                    <label for="filter1">Hertog Jan</label><br>
+                </div>
+                <div class="filters">
+                    <input type="checkbox" id="filter2" name="filter2" value="Hertog jan">
+                    <label for="filter2">Leffe</label><br>
+                </div>
+                <div class="filters">
+                    <input type="checkbox" id="filter3" name="filter3" value="Hertog jan">
+                    <label for="filter3">La Chouffe</label><br>
+                </div>
+                <div class="filters">
+                    <input type="checkbox" id="filter4" name="filter4" value="Hertog jan">
+                    <label for="filter4">Apple Bandit</label><br>
+                </div>
+                <div class="filters">
+                    <input type="checkbox" id="filter5" name="filter5" value="Hertog jan">
+                    <label for="filter5">La Chouffe</label><br>
+                </div>
+            </div>
+            <div class="category">
+                <p>Categorie</p>
+                <div class="filters">
+                    <input type="radio" id="cat1" name="cat" value="Hertog jan">
+                    <label for="cat1">Pils</label><br>
+                </div>
+                <div class="filters">
+                    <input type="radio" id="cat2" name="cat" value="Hertog jan">
+                    <label for="cat2">Speciaalbier</label><br>
+                </div>
+                <div class="filters">
+                    <input type="radio" id="cat3" name="cat" value="Hertog jan">
+                    <label for="cat3">Alcoholvrij</label><br>
+                </div>
+            </div>
+        </aside>
 
         <div class="products">
             <div class="product">
                 <div class="image">
                     <img src="\assets\product1.png" alt="product1">
                 </div>
-                <div class="productinfo">
-                    <div class="price">
-                        <span>1.</span>
-                        <sup>49</sup>
+                <div class="info">
+                    <div class="productinfo">
+                        <div class="price">
+                            <span>1.</span>
+                            <sup>49</sup>
+                        </div>
+                        <p>Hertog Jan</p>
+                        <strong>Lentebock</strong>
                     </div>
-                    <p>Hertog Jan</p>
-                    <strong>Lentebock</strong>
+                    <div class="cart">
+                        <svg class="cart-logo" fill="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>
+                    </div>
                 </div>
             </div>
             <div class="product">
                 <div class="image">
                     <img src="\assets\product2.png" alt="product2">
                 </div>
-                <div class="productinfo">
-                    <div class="price">
-                        <span>1.</span>
-                        <sup>49</sup>
+                <div class="info">
+                    <div class="productinfo">
+                        <div class="price">
+                            <span>1.</span>
+                            <sup>30</sup>
+                        </div>
+                        <p>Hertog Jan</p>
+                        <strong>Bockbier</strong>
                     </div>
-                    <p>Hertog Jan</p>
-                    <strong>Lentebock</strong>
+                    <div class="cart">
+                        <svg class="cart-logo" fill="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>
+                    </div>
                 </div>
             </div>
             <div class="product">
                 <div class="image">
                     <img src="\assets\product3.png" alt="product3">
                 </div>
-                <div class="productinfo">
-                    <div class="price">
-                        <span>1.</span>
-                        <sup>49</sup>
+                <div class="info">
+                    <div class="productinfo">
+                        <div class="price">
+                            <span>1.</span>
+                            <sup>49</sup>
+                        </div>
+                        <p>Hertog Jan</p>
+                        <strong>Karakter</strong>
                     </div>
-                    <p>Hertog Jan</p>
-                    <strong>Lentebock</strong>
+                    <div class="cart">
+                        <svg class="cart-logo" fill="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>
+                    </div>
                 </div>
             </div>
             <div class="product">
                 <div class="image">
                     <img src="\assets\product4.png" alt="product4">
                 </div>
-                <div class="productinfo">
-                    <div class="price">
-                        <span>1.</span>
-                        <sup>49</sup>
+                <div class="info">
+                    <div class="productinfo">
+                        <div class="price">
+                            <span>2.</span>
+                            <sup>10</sup>
+                        </div>
+                        <p>Leffe</p>
+                        <strong>Tripel-Tripel</strong>
                     </div>
-                    <p>Hertog Jan</p>
-                    <strong>Lentebock</strong>
+                    <div class="cart">
+                        <svg class="cart-logo" fill="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>
+                    </div>
                 </div>
             </div>
             <div class="product">
                 <div class="image">
                     <img src="\assets\product5.png" alt="product5">
                 </div>
-                <div class="productinfo">
-                    <div class="price">
-                        <span>1.</span>
-                        <sup>49</sup>
+                <div class="info">
+                    <div class="productinfo">
+                        <div class="price">
+                            <span>1.</span>
+                            <sup>75</sup>
+                        </div>
+                        <p>La Chouffe</p>
+                        <strong>Blonde blond</strong>
                     </div>
-                    <p>Hertog Jan</p>
-                    <strong>Lentebock</strong>
+                    <div class="cart">
+                        <svg class="cart-logo" fill="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>
+                    </div>
                 </div>
             </div>
             <div class="product">
                 <div class="image">
                     <img src="\assets\product6.png" alt="product6">
                 </div>
-                <div class="productinfo">
-                    <div class="price">
-                        <span>1.</span>
-                        <sup>49</sup>
+                <div class="info">
+                    <div class="productinfo">
+                        <div class="price">
+                            <span>1.</span>
+                            <sup>69</sup>
+                        </div>
+                        <p>Apple Bandit</p>
+                        <strong>Cider</strong>
                     </div>
-                    <p>Hertog Jan</p>
-                    <strong>Lentebock</strong>
+                    <div class="cart">
+                        <svg class="cart-logo" fill="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>
+                    </div>
                 </div>
             </div>
             <div class="product">
                 <div class="image">
                     <img src="\assets\product7.png" alt="product7">
                 </div>
-                <div class="productinfo">
-                    <div class="price">
-                        <span>1.</span>
-                        <sup>49</sup>
+                <div class="info">
+                    <div class="productinfo">
+                        <div class="price">
+                            <span>1.</span>
+                            <sup>79</sup>
+                        </div>
+                        <p>La Trappe</p>
+                        <strong>Dubbel</strong>
                     </div>
-                    <p>Hertog Jan</p>
-                    <strong>Lentebock</strong>
+                    <div class="cart">
+                        <svg class="cart-logo" fill="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>
+                    </div>
                 </div>
             </div>
             <div class="product">
                 <div class="image">
                     <img src="\assets\product8.png" alt="product8">
                 </div>
-                <div class="productinfo">
-                    <div class="price">
-                        <span>1.</span>
-                        <sup>49</sup>
+                <div class="info">
+                    <div class="productinfo">
+                        <div class="price">
+                            <span>1.</span>
+                            <sup>99</sup>
+                        </div>
+                        <p>Texels</p>
+                        <strong>Skuumkoppe</strong>
                     </div>
-                    <p>Hertog Jan</p>
-                    <strong>Lentebock</strong>
+                    <div class="cart">
+                        <svg class="cart-logo" fill="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.53 7l-.564 2h-15.127l-.839-2h16.53zm-14.013 6h12.319l.564-2h-13.722l.839 2zm5.983 5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm11.305-15l-3.432 12h-13.017l.839 2h13.659l3.474-12h1.929l.743-2h-4.195zm-6.305 15c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg>
+                    </div>
                 </div>
-                <div></div>
             </div>
         </div>
     </main>
@@ -117,12 +193,42 @@
 
 <style lang="scss">
 @import "../styles/variables.scss";
-main{
+.productpage{
+    width: 100%;
     display: flex;
-    .filter
+    aside
     {
+        text-align: left;
         width: 20%;
-        background-color: red;
+        padding-left: 3rem;
+        padding-top: 2rem;
+        // background-color: red;
+        h2 {
+            font-size: 3.2rem; 
+        }
+        .brands, .category{
+            padding-top: 1rem;
+            p {
+                font-size: 1.2rem;
+            }
+            .filters {
+                padding-top: 10px;
+                font-weight: 400;
+                display: flex;
+                align-items: center;
+                input[type=checkbox], input[type=radio] {
+                    transform: scale(1.3);
+                    margin-left: 2px;
+                    accent-color: rgb(207,132,48);
+                }
+                label {
+                    margin-left: 0.5rem;
+                }
+            }
+        }
+        .category {
+            padding-top: 2rem;
+        }
     }
 
 
@@ -137,7 +243,8 @@ main{
         .product {
             height: 20rem;
             width: 17rem;
-            background-color: grey;
+            // background-color: grey;
+            
             img {   
                 width: 4rem;  
                 height: 13.5rem;
@@ -146,16 +253,41 @@ main{
             .image{
                 text-align: center;
             }
-            .productinfo{
-                margin-left: 1rem;
-                .price {
-                    font-size: 2rem;
-                    padding-left: 7px;
+            .info {
+                display: flex;
+                align-items: center;
+                .cart {
+                    flex: 20%;
+                    display: flex;
+                    margin-right: 1rem;
+                    width: 3rem;
+                    height: 3rem;
+                    background-color: rgb(207,132,48);
+                    align-items: center;
+                    border-radius: 0.2rem;
+                    .cart-logo {
+                        margin: auto;
+                        display: block;
+                    }
                 }
-                strong {
-                    font-weight: 900;
+
+                .productinfo{
+                    flex: 80%;
+                    margin-left: 1rem;
+                    p {
+                        font-weight:500;
+                    }
+                    .price {
+                        font-size: 2rem;
+                        padding-left: 7px;
+                        font-weight:500;
+                    }
+                    strong {
+                        font-weight: 900;
+                    }
                 }
             }
+            
             
         }
     }
